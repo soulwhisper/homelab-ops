c4730c874d9f551f5da7f4df5f167488a2786391
Hekatos Noir <soulwhisper@outlook.com>
2025-05-14 15:35:22 +0800

    feat(infra): add vyos config

---
+++ infrastructure/router/config/dhcp.sh
@@ -0,0 +1,14 @@
+#!/bin/vbash
+
+# DHCP Server
+set service dhcp-server hostfile-update
+set service dhcp-server shared-network-name HOMELAB authoritative
+set service dhcp-server shared-network-name HOMELAB option domain-name 'homelab.internal'
+set service dhcp-server shared-network-name HOMELAB option domain-search 'homelab.internal'
+
+## LAB
+set service dhcp-server shared-network-name LAB subnet 10.10.0.0/24 subnet-id '1'
+set service dhcp-server shared-network-name LAB subnet 10.10.0.0/24 option default-router '10.10.0.1'
+set service dhcp-server shared-network-name LAB subnet 10.10.0.0/24 option name-server '10.10.0.1'
+set service dhcp-server shared-network-name LAB subnet 10.10.0.0/24 range 0 start '10.10.0.101'
+set service dhcp-server shared-network-name LAB subnet 10.10.0.0/24 range 0 stop '10.10.0.250'
---
+++ infrastructure/router/config/misc.sh
@@ -0,0 +1,31 @@
+#!/bin/vbash
+
+# System
+set system host-name 'vyos'
+set system domain-name 'homelab.internal'
+set system time-zone 'Asia/Shanghai'
+set system name-server '10.0.0.1'
+
+set protocols static route 0.0.0.0/0 next-hop 10.0.0.1
+
+# NTP
+delete service ntp allow-client
+delete service ntp server
+set service ntp server 'time.cloudflare.com' nts
+set service ntp listen-address '10.10.0.1'
+set service ntp allow-client address '10.10.0.0/24'
+
+# SSH
+set service ssh port '22'
+set service ssh listen-address '10.0.10.10'
+set service ssh disable-host-validation
+set service ssh disable-password-authentication
+
+set system login user vyos authentication public-keys 'Default' key "AAAAC3NzaC1lZDI1NTE5AAAAIB16hkZ4PM3ucd6spFVd2J2YB4NGpuuiXGM+gmL6mbpO"
+set system login user vyos authentication public-keys 'Default' type ssh-ed25519
+
+# Misc
+set system option startup-beep
+set system option reboot-on-panic
+set system option root-partition-auto-resize
+set system option performance latency
