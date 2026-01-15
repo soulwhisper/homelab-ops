a317994eddf12699e7bdd9d01a6f6c262fddc7ec
Hekatos Noir <soulwhisper@outlook.com>
2026-01-15 10:16:12 +0800

    patch(infra): update switch config

--- infrastructure/switch/init.sh
+++ infrastructure/switch/init.sh
@@ -2,12 +2,28 @@
 # This script configures H3C S6520-24S-SI as core switch for a homelab environment
 # If pasted into the switch console, make sure to paste by phases to avoid errors
 
+# reset
+display default-configuration
+restore factory-default
+reboot
+
+# defaults
+system-view
 sysname core-switch
+
 ntp-service enable
 clock timezone Beijing add 08:00:00
 clock protocol ntp
 ntp-service unicast-server 10.10.0.254
 
+interface loopback 0
+ ip address 10.255.255.1 255.255.255.255
+ bfd min-transmit-interval 400
+ bfd min-receive-interval 400
+ bfd detect-multiplier 5
+router id 10.255.255.1
+
+# security
 interface m-gigabitethernet 0/0/0
  ip address 10.0.0.2 24
 
@@ -20,7 +36,7 @@ public-key local create rsa
 ssh server enable
 
 acl advanced 3000
- rule 0 permit ip destination 10.0.0.2 0
+ rule 0 permit ip source 10.0.0.0 0.0.0.255 destination 10.0.0.2 0
  rule 100 deny ip
 
 line vty 0 63
@@ -28,41 +44,39 @@ line vty 0 63
  acl 3000 inbound
  user-role network-admin
 
-password-recovery enable
-link-aggregation global load-sharing mode source-ip destination-ip source-mac destination-mac source-port destination-port
-ipv6 nd detection port-match-ignore
-
-bfd session init-mode active
-
-dhcp enable
+# snooping
 dhcp snooping enable
-ipv6 dhcp snooping enable
+# ipv6 nd detection port-match-ignore
 
-multicast routing
-
-stp global enable
+# stp
 stp mode rstp
 stp instance 0 root primary
+stp bpdu-protection
 
 # vlan
 vlan 100
 dhcp snooping binding record
-ipv6 dhcp snooping binding record
-ipv6 nd detection enable
 
 # eBGP with BFD & OSPF
+bfd session init-mode active
 bgp 65000
- router-id 10.10.0.1
- timer keepalive 60 hold 180
+ router-id 10.255.255.1
+ timer keepalive 10 hold 30
  group k8s external
   peer k8s bfd
   peer k8s as-number 65100
-  peer k8s connect-interface vlan-interface 100
+  peer k8s connect-interface loopback 0
  peer 10.10.0.101 group k8s
  peer 10.10.0.102 group k8s
  peer 10.10.0.103 group k8s
  address-family ipv4
   peer k8s enable
+
+ospf 1 router-id 10.255.255.1
+ silent-interface interface vlan-interface 100
+ area 0
+  network 10.255.255.1 0.0.0.0
+  network 10.10.0.0 0.0.0.255
 
 # LACP to K8S
 interface bridge-aggregation 10
@@ -71,12 +85,7 @@ interface bridge-aggregation 10
  port link-type access
  port access vlan 100
  jumboframe enable
- lacp edge-port
  stp edged-port
- dhcp snooping rate-limit 64
- dhcp snooping check request-message
- ipv6 dhcp snooping rate-limit 64
- ipv6 dhcp snooping check request-message
  ipv6 nd raguard role host
 
 interface bridge-aggregation 20
@@ -85,12 +94,7 @@ interface bridge-aggregation 20
  port link-type access
  port access vlan 100
  jumboframe enable
- lacp edge-port
  stp edged-port
- dhcp snooping rate-limit 64
- dhcp snooping check request-message
- ipv6 dhcp snooping rate-limit 64
- ipv6 dhcp snooping check request-message
  ipv6 nd raguard role host
 
 interface bridge-aggregation 30
@@ -99,12 +103,7 @@ interface bridge-aggregation 30
  port link-type access
  port access vlan 100
  jumboframe enable
- lacp edge-port
  stp edged-port
- dhcp snooping rate-limit 64
- dhcp snooping check request-message
- ipv6 dhcp snooping rate-limit 64
- ipv6 dhcp snooping check request-message
  ipv6 nd raguard role host
 
 interface ten-gigabitethernet 1/0/1
@@ -132,12 +131,7 @@ interface bridge-aggregation 70
  port link-type access
  port access vlan 100
  jumboframe enable
- lacp edge-port
  stp edged-port
- dhcp snooping rate-limit 64
- dhcp snooping check request-message
- ipv6 dhcp snooping rate-limit 64
- ipv6 dhcp snooping check request-message
  ipv6 nd raguard role host
 
 interface ten-gigabitethernet 1/0/13
@@ -148,17 +142,12 @@ interface ten-gigabitethernet 1/0/14
 
 # to Workstation-Fiber, LACP 90
 interface bridge-aggregation 90
- description workstation
+ description workstation-fiber
  link-aggregation mode dynamic
  port link-type access
  port access vlan 100
  jumboframe enable
- lacp edge-port
  stp edged-port
- dhcp snooping rate-limit 64
- dhcp snooping check request-message
- ipv6 dhcp snooping rate-limit 64
- ipv6 dhcp snooping check request-message
  ipv6 nd raguard role host
 
 interface ten-gigabitethernet 1/0/17
@@ -172,10 +161,8 @@ interface ten-gigabitethernet 1/0/24
  description router-trunk
  port link-type trunk
  port trunk permit vlan 1 10 100 200 210
- stp edged-port
+ stp point-to-point force-true
  dhcp snooping trust
- ipv6 dhcp snooping trust
- ipv6 nd detection trust
  ipv6 nd raguard role router
 
 # VIF
@@ -183,20 +170,6 @@ interface vlan-interface 100
  description lab
  ip address 10.10.0.1 24
  ipv6 address auto
- bfd min-transmit-interval 400
- bfd min-receive-interval 400
- bfd detect-multiplier 5
 
 # Routing
-ip route-static 0.0.0.0 0 10.10.0.254
-
-# : save
-display current-configuration diff
-save safely main
-
-# : check
-display link-aggregation summary
-display bfd session
-display bgp peer
-display stp brief
-display vlan
+# ip route-static 0.0.0.0 0 10.10.0.254
