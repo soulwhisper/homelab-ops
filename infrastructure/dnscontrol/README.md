dc08434d6d44d972776479e40a7e1dd5c676d9a1
Hekatos Noir <soulwhisper@outlook.com>
2026-01-19 10:12:58 +0800

    feat(infra): add dnscontrol

---
+++ infrastructure/dnscontrol/README.md
@@ -0,0 +1,17 @@
+## DNS
+
+```shell
+brew install dnscontrol
+dnscontrol [command]
+
+docker run --rm -it -v "$(pwd):/dns" ghcr.io/stackexchange/dnscontrol [command]
+
+pnpm install
+pnpm run lint
+
+dnscontrol write-types
+
+dnscontrol check
+dnscontrol preview
+dnscontrol push
+```
---
+++ infrastructure/dnscontrol/dnsconfig.js
@@ -0,0 +1,37 @@
+// @ts-check
+/// <reference path="types-dnscontrol.d.ts" />
+
+var REG_NONE = NewRegistrar("none");
+var DSP_ADGUARDHOME = NewDnsProvider("adguard_home");
+
+var CLUSTER_NODES = [
+  { name: "exarch-01", ip: "10.10.0.101", type: "cp" },
+  { name: "exarch-02", ip: "10.10.0.102", type: "cp" },
+  { name: "exarch-03", ip: "10.10.0.103", type: "cp" },
+];
+
+/**
+ * @param {string} endpointName
+ * @param {Array<{name: string, ip: string, type: string}>} nodes
+ * @returns {Array<DomainModifier>}
+ */
+function generateClusterRecords(endpointName, nodes) {
+  var records = [];
+  for (var i = 0; i < nodes.length; i++) {
+    var node = nodes[i];
+    records.push(A(node.name, node.ip));
+    if (node.type === "cp") {
+      records.push(A(endpointName, node.ip));
+    }
+  }
+  return records;
+}
+
+D(
+  "homelab.internal",
+  REG_NONE,
+  DnsProvider(DSP_ADGUARDHOME),
+  A("nas", "10.10.0.10"),
+  A("zigbee", "10.10.0.20"),
+  ...generateClusterRecords("k8s", CLUSTER_NODES)
+);
